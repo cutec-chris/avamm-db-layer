336b5bcbc00ca36b6fb33e85ea0b2eec27075cbc
Christian U <info@cu-tec.de>
2018-08-27 17:16:19 +0200

    Ability to turn off Parametrizing

--- uabstractdblayer.pas
+++ uabstractdblayer.pas
@@ -58,6 +58,7 @@ type
     FTriggers: TStrings;
     FProperties: String;
     FDatabaseDir : string;
+    FUseParameters: Boolean;
     FUsersFilter: string;
     FUseExtData : Boolean;
     FMainConnection: TAbstractDBConnection;
@@ -90,7 +91,7 @@ type
     property Tables : TStrings read FTables;
     property Triggers : TStrings read FTriggers;
     function DBExists : Boolean;
-    function GetFullTableName(aTable: string): string;virtual;
+    function GetFullTableName(aTable: string;DoLookup : Boolean = True): string;virtual;
     property LimitAfterSelect : Boolean read GetLimitAfterSelect;
     property LimitSTMT : string read GetLimitSTMT;
     function IsSQLDB : Boolean;virtual;
@@ -128,6 +129,7 @@ type
 
     property LastStatement : string read FLastStmt write FLastStmt;
     property LastTime : Int64 read FLastTime write FLastTime;
+    property ParameteriseSQL : Boolean read FUseParameters write FUseParameters;
     property CriticalSection : TCriticalSection read FCS;
     property IgnoreOpenRequests : Boolean read FIgnoreOpenrequests write FIgnoreOpenrequests;
     property UsersFilter : string read FUsersFilter write FUsersFilter;
@@ -202,7 +204,8 @@ begin
   Result := TableExists('USERS') and TableExists('GEN_SQL_ID') and TableExists('GEN_AUTO_ID');
 end;
 
-function TAbstractDBModule.GetFullTableName(aTable: string): string;
+function TAbstractDBModule.GetFullTableName(aTable: string; DoLookup: Boolean
+  ): string;
 begin
   Result := aTable;
 end;
@@ -263,6 +266,7 @@ begin
   FCS := TCriticalSection.Create;
   FIgnoreOpenrequests := False;
   FUseExtData:=False;
+  FUseParameters:=True;
 end;
 
 destructor TAbstractDBModule.Destroy;
@@ -813,25 +817,29 @@ begin
   i := 0;
   NewSQL := '';
   Parameters.Clear;
-  while pos(aQuotes,aSQL)>0 do
+  if FUseParameters then
     begin
-      NewSQL:=NewSQL+copy(aSQL,0,pos(aQuotes,aSQL)-1);
-      aSQL := copy(aSQL,pos(aQuotes,aSQL)+1,length(aSQL));
-      NewSQL:=NewSQL+':Param'+IntToStr(i);
-      aParamCont := copy(aSQL,0,pos(aQuotes,aSQL)-1);
-      aSQL := copy(aSQL,pos(aQuotes,aSQL)+1,length(aSQL));
-      if copy(aSQL,0,1)=aQuotes then
+      while pos(aQuotes,aSQL)>0 do
         begin
-          aParamCont += aQuotes+copy(aSQL,0,pos(aQuotes,aSQL)-1);
+          NewSQL:=NewSQL+copy(aSQL,0,pos(aQuotes,aSQL)-1);
           aSQL := copy(aSQL,pos(aQuotes,aSQL)+1,length(aSQL));
-          aParamCont += aQuotes+copy(aSQL,0,pos(aQuotes,aSQL)-1);
+          NewSQL:=NewSQL+':Param'+IntToStr(i);
+          aParamCont := copy(aSQL,0,pos(aQuotes,aSQL)-1);
           aSQL := copy(aSQL,pos(aQuotes,aSQL)+1,length(aSQL));
+          if copy(aSQL,0,1)=aQuotes then
+            begin
+              aParamCont += aQuotes+copy(aSQL,0,pos(aQuotes,aSQL)-1);
+              aSQL := copy(aSQL,pos(aQuotes,aSQL)+1,length(aSQL));
+              aParamCont += aQuotes+copy(aSQL,0,pos(aQuotes,aSQL)-1);
+              aSQL := copy(aSQL,pos(aQuotes,aSQL)+1,length(aSQL));
+            end;
+          Parameters.Values['Param'+IntToStr(i)]:=aParamCont;
+          NewSQL:=NewSQL;
+          inc(i);
         end;
-      Parameters.Values['Param'+IntToStr(i)]:=aParamCont;
-      NewSQL:=NewSQL;
-      inc(i);
-    end;
-  NewSQL:=NewSQL+aSQL;
+      NewSQL:=NewSQL+aSQL;
+    end
+  else NewSQL:=aSQL;
 end;
 
 function TAbstractDBModule.CheckForInjection(aFilter: string): Boolean;
